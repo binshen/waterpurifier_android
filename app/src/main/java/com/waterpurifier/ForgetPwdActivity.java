93205816ec8a1d7072f36da19e98a0f8dc074d2f
bin.shen <bluelazysb@hotmail.com>
2016-12-04 22:42:56 +0800

    updates

--- app/src/main/java/com/waterpurifier/ForgetPwdActivity.java
+++ app/src/main/java/com/waterpurifier/ForgetPwdActivity.java
@@ -3,15 +3,32 @@ package com.waterpurifier;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.waterpurifier.base.BaseActivity;
 
 public class ForgetPwdActivity extends BaseActivity implements View.OnClickListener {
 
+    private ImageView mIvLeft;
+    private TextView mTvTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pwd);
+
+        mTvTitle = (TextView)findViewById(R.id.tv_title);
+        mTvTitle.setText("忘记密码");
+
+        mIvLeft = (ImageView)findViewById(R.id.btn_title_left);
+        mIvLeft.setImageResource(R.mipmap.back);
+        mIvLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/waterpurifier/RegisterActivity.java
+++ app/src/main/java/com/waterpurifier/RegisterActivity.java
@@ -2,15 +2,32 @@ package com.waterpurifier;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.waterpurifier.base.BaseActivity;
 
 public class RegisterActivity extends BaseActivity implements View.OnClickListener {
 
+    private ImageView mIvLeft;
+    private TextView mTvTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTvTitle = (TextView)findViewById(R.id.tv_title);
+        mTvTitle.setText("注册");
+
+        mIvLeft = (ImageView)findViewById(R.id.btn_title_left);
+        mIvLeft.setImageResource(R.mipmap.back);
+        mIvLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
