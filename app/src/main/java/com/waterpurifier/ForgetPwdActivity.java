c74bac07738d351bb3b6114d6371094cec14c65b
bin.shen <bluelazysb@hotmail.com>
2016-12-04 21:10:29 +0800

    updates

--- app/src/main/java/com/waterpurifier/ForgetPwdActivity.java
+++ app/src/main/java/com/waterpurifier/ForgetPwdActivity.java
@@ -1,16 +1,17 @@
 package com.waterpurifier;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.waterpurifier.base.BaseActivity;
 
-public class RegisterActivity extends BaseActivity implements View.OnClickListener {
+public class ForgetPwdActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_forget_pwd);
     }
 
     @Override
