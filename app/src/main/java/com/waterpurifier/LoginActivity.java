0cdece101d6d17bf5c004b48ea3b4a3d39823546
bin.shen <bluelazysb@hotmail.com>
2016-12-04 20:29:30 +0800

    updates

--- app/src/main/java/com/waterpurifier/LoginActivity.java
+++ app/src/main/java/com/waterpurifier/LoginActivity.java
@@ -1,14 +1,57 @@
 package com.waterpurifier;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.waterpurifier.base.BaseActivity;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    private EditText mEtTel;
+    private EditText mEtPwd;
+    private Button mBtnLogin;
+    private Button mBtnRegister;
+    private TextView mTvForgetpwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEtTel = (EditText)findViewById(R.id.et_tel);
+        mEtPwd = (EditText)findViewById(R.id.et_psw);
+        mBtnLogin = (Button)findViewById(R.id.btn_login);
+        mBtnRegister = (Button)findViewById(R.id.btn_register);
+        mTvForgetpwd = (TextView)findViewById(R.id.tv_forget_psw);
+
+        mTvForgetpwd.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        mTvForgetpwd.setOnClickListener(this);
+        mBtnLogin.setOnClickListener(this);
+        mBtnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String tel = mEtTel.getText().toString().trim();
+        String psw = mEtPwd.getText().toString().trim();
+        switch (v.getId()) {
+            case R.id.tv_forget_psw:
+                Toast.makeText(getApplicationContext(), "mTvForgetpwd", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btn_login:
+                Toast.makeText(getApplicationContext(), "mBtnLogin", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btn_register:
+                Toast.makeText(getApplicationContext(), "mBtnRegister", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
