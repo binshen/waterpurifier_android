bf0fd43f4d2d7a83e0ae88151e9ac72050297f17
bin.shen <bluelazysb@hotmail.com>
2016-12-07 21:47:57 +0800

    updates

--- app/src/main/java/com/waterpurifier/fragment/DiscoverFragment.java
+++ app/src/main/java/com/waterpurifier/fragment/DiscoverFragment.java
@@ -1,12 +1,16 @@
 package com.waterpurifier.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
@@ -37,6 +41,14 @@ public class DiscoverFragment extends BaseFragment implements BaseSliderView.OnS
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        RelativeLayout head_layout = (RelativeLayout) view.findViewById(R.id.head_layout);
+        head_layout.setBackgroundResource(R.mipmap.head_discover);
+        ImageView btn_head_left = (ImageView) view.findViewById(R.id.btn_head_left);
+        btn_head_left.setVisibility(View.INVISIBLE);
+        TextView tv_head_title = (TextView) view.findViewById(R.id.tv_head_title);
+        tv_head_title.setText("发 现");
+        tv_head_title.setTextColor(Color.WHITE);
 
         mDemoSlider = (SliderLayout) view.findViewById(R.id.slider);
 
