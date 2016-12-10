9656866b3d7fd3b943f996566e6a47a582f0202c
沈斌 <bluelazysb@hotmail.com>
2016-12-10 10:51:12 +0800

    updates

--- app/src/main/java/com/waterpurifier/fragment/DeviceMainFragment.java
+++ app/src/main/java/com/waterpurifier/fragment/DeviceMainFragment.java
@@ -1,12 +1,16 @@
 package com.waterpurifier.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.waterpurifier.R;
 import com.waterpurifier.base.BaseFragment;
@@ -26,6 +30,12 @@ public class DeviceMainFragment extends BaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-
+        RelativeLayout head_layout = (RelativeLayout) view.findViewById(R.id.head_layout);
+        head_layout.setBackgroundResource(R.mipmap.head_discover);
+        ImageView btn_head_left = (ImageView) view.findViewById(R.id.btn_head_left);
+        btn_head_left.setVisibility(View.INVISIBLE);
+        TextView tv_head_title = (TextView) view.findViewById(R.id.tv_head_title);
+        tv_head_title.setText("智能净水器");
+        tv_head_title.setTextColor(Color.WHITE);
     }
 }
